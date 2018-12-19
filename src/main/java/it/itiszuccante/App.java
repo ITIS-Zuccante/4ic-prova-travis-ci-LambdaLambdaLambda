b9d9501b55ef585a01aa01b8d28ba5f1d95d4e11
Alberto Carraro <22873096+LambdaLambdaLambda@users.noreply.github.com>
2018-12-19 13:36:37 +0100

    Update App.java

--- src/main/java/it/itiszuccante/App.java
+++ src/main/java/it/itiszuccante/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
+        System.out.println( "La 4 ID è una classe buona." );
     }
 }
